ec06c4ebce58db5aecde31b14ead1d7e490d07f4
chenkaixu <chenkaixusan@gmail.com>
2025-04-25 13:01:04 +0900

    save yolo res to .pt and .json file

--- prepare_dataset/main.py
+++ prepare_dataset/main.py
@@ -28,6 +28,7 @@ import logging
 from pathlib import Path
 import hydra
 
+import torch
 from torchvision.io import read_video
 
 from project.utils import del_folder, make_folder
@@ -47,12 +48,12 @@ def process(parames, person: str):
     # prepare the preprocess
     preprocess = Preprocess(parames)
 
-    res = dict()
+    res = {}
 
     one_person = RAW_PATH / person
 
     for one_video in one_person.iterdir():
-        vframes, audio, _ = read_video(one_video, pts_unit="sec", output_format="TCHW")
+        vframes, _, _ = read_video(one_video, pts_unit="sec", output_format="TCHW")
 
         # * step3: use preprocess to get information.
         # the format is: final_frames, bbox_none_index, label, optical_flow, bbox, mask, pose
@@ -77,21 +78,20 @@ def process(parames, person: str):
             "img_shape": (vframes.shape[2], vframes.shape[3]),
             "frame_count": vframes.shape[0],
             "none_index": bbox_none_index,
-            "bbox": bbox.tolist(),  # serialized as list
-            "mask": mask.tolist(),
+            "bbox": bbox,  # serialized as list
+            "mask": mask,
             # "optical_flow": optical_flow.tolist(),
             "keypoint": {
-                "keypoint": keypoints.tolist(),
-                "keypoint_score": keypoints_score.tolist(),
+                "keypoint": keypoints,
+                "keypoint_score": keypoints_score,
             },
         }
 
         res[one_video.name] = sample_json_info
 
     # * step5: save the video frames to json file
-    SAVE_PATH.mkdir(parents=True, exist_ok=True)
-
     save_to_json(res, SAVE_PATH, person)  # save the sample info to json file.
+    save_to_pt(res, SAVE_PATH, person)  # save the sample info to json file.
 
 
 def save_to_json(sample_info: dict, save_path: Path, person: str) -> None:
@@ -104,19 +104,42 @@ def save_to_json(sample_info: dict, save_path: Path, person: str) -> None:
     """
 
     for k, v in sample_info.items():
-        save_path_with_name = save_path / person / (k.split(".")[0] + ".json")
+        save_path_with_name = save_path / "json" / person / (k.split(".")[0] + ".json")
 
         make_folder(save_path_with_name.parent)
 
         # convert Path to str.
+        v["bbox"] = v["bbox"].tolist()  # serialized as list
+        v["mask"] = v["mask"].tolist()
+        v["keypoint"]["keypoint"] = v["keypoint"]["keypoint"].tolist()
+        v["keypoint"]["keypoint_score"] = v["keypoint"]["keypoint_score"].tolist()
 
         with open(save_path_with_name, "w") as f:
-            json.dump(sample_info, f, indent=4)
+            json.dump(v, f, indent=4)
 
-        logging.info(f"Save the {sample_info['video_name']} to {save_path_with_name}")
+        logging.info(f"Save the {v['video_name']} to {save_path_with_name}")
 
 
-@hydra.main(config_path="../configs/", config_name="prepare_dataset")
+def save_to_pt(sample_info: dict, save_path: Path, person: str) -> None:
+    """save the sample info to json file.
+
+    Args:
+        sample_info (dict): _description_
+        save_path (Path): _description_
+        logger (logging): _description_
+    """
+
+    for k, v in sample_info.items():
+        save_path_with_name = save_path / "pt" / person / (k.split(".")[0] + ".pt")
+
+        make_folder(save_path_with_name.parent)
+
+        torch.save(v, save_path_with_name)
+
+        logging.info(f"Save the {v['video_name']} to {save_path_with_name}")
+
+
+@hydra.main(config_path="../configs/", config_name="prepare_dataset", version_base=None)
 def main(parames):
     """
     main, for the multiprocessing using.
