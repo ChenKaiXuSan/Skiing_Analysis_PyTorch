89804c7db51808dbfd24a86a03d78963dfeb00d4
chenkaixu <chenkaixusan@gmail.com>
2025-06-01 03:33:20 +0000

    world 3d pose

---
+++ project/convert_3d.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+File: /workspace/code/project/convert_3d.py
+Project: /workspace/code/project
+Created Date: Sunday June 1st 2025
+Author: Kaixu Chen
+-----
+Comment:
+
+Have a good code time :)
+-----
+Last Modified: Sunday June 1st 2025 12:23:57 pm
+Modified By: the developer formerly known as Kaixu Chen at <chenkaixusan@gmail.com>
+-----
+Copyright (c) 2025 The University of Tsukuba
+-----
+HISTORY:
+Date      	By	Comments
+----------	---	---------------------------------------------------------
+'''
