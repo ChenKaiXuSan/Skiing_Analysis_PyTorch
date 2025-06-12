fe3115ceeb9ab19e222568826eb18f4b80c4ac4c
chenkaixu <chenkaixusan@gmail.com>
2025-06-12 13:00:11 +0000

    run videopose 3d code

---
+++ VideoPose3D/common/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+'''
+File: /workspace/code/VideoPose3D/common/__init__.py
+Project: /workspace/code/VideoPose3D/common
+Created Date: Thursday June 12th 2025
+Author: Kaixu Chen
+-----
+Comment:
+
+Have a good code time :)
+-----
+Last Modified: Thursday June 12th 2025 9:38:16 pm
+Modified By: the developer formerly known as Kaixu Chen at <chenkaixusan@gmail.com>
+-----
+Copyright (c) 2025 The University of Tsukuba
+-----
+HISTORY:
+Date      	By	Comments
+----------	---	---------------------------------------------------------
+'''
